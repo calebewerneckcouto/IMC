d911db8861a4d82ee5fd47c00eaecbd392a79070
Calebe <calebewerneck@hotmail.com>
2024-04-16 15:14:55 -0300

    JWT TOKEN SWAGGER CONFIGURAÇÂO

--- src/main/java/br/com/imc/SwaggerConfig.java
+++ src/main/java/br/com/imc/SwaggerConfig.java
@@ -1,6 +1,8 @@
 package br.com.imc;
 
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.context.annotation.Bean;
@@ -14,9 +16,13 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.builders.ResponseMessageBuilder;
 import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.Contact;
 import springfox.documentation.service.ResponseMessage;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -25,20 +31,29 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfig {
 	
 	@Bean
-	public Docket apiDocSwagger() {
+	public Docket apiDocSwgger() {
 		
 		return new Docket(DocumentationType.SWAGGER_2)
 				.select()
 				.apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
 				.paths(PathSelectors.any())
 				.build().useDefaultResponseMessages(false)
-				.globalResponseMessage(RequestMethod.GET,this.responseCodeHttp())
-				.apiInfo(ApiInfo());
+				.globalResponseMessage(RequestMethod.GET, responseCodeHttp())
+				.apiInfo(apiInfo())
+				.securityContexts(securityContext())
+				.securitySchemes(listApiKey());
 		
 	}
 	
+	private List<ApiKey> listApiKey(){
+		List<ApiKey> apiKeys = new ArrayList<ApiKey>();
+		apiKeys.add(new ApiKey("JWT", "Authorization", "header"));
+		return apiKeys;
+	}
 	
-	private List<ResponseMessage>responseCodeHttp(){
+	
+	private List<ResponseMessage> responseCodeHttp(){
+		 
 		List<ResponseMessage> list = new ArrayList<ResponseMessage>();
 		
 		list.add(new ResponseMessageBuilder()
@@ -46,8 +61,6 @@ public class SwaggerConfig {
 				.message("Erro interno do servidor")
 				.responseModel(new ModelRef("Error"))
 				.build());
-		
-	
 		
 		
 		list.add(new ResponseMessageBuilder()
@@ -59,34 +72,47 @@ public class SwaggerConfig {
 		
 		list.add(new ResponseMessageBuilder()
 				.code(401)
-				.message("Não Autorizado")
+				.message("Não autorizado")
 				.responseModel(new ModelRef("Error"))
 				.build());
+		
+		
 		
 		
 		list.add(new ResponseMessageBuilder()
 				.code(403)
-				.message("Acesso Negado")
+				.message("Acesso negado")
 				.responseModel(new ModelRef("Error"))
 				.build());
 		
+		
 		return list;
-		
-		
 	}
+
 	
-	
-	private ApiInfo  ApiInfo() {
-		
-		return new ApiInfoBuilder().title("Sistema para controle de IMC")
-				.description("Sistema CWC")
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("Sistema de Calculo de IMC ")
+				.description("IMC")
 				.version("0.0.1")
 				.licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
-				.contact(new Contact("Calebe Werneck Couto", "http://cwc3d.net", "calebewerneck@hotmail.com"))
+				.license("Apache License")
+				.contact(new Contact("Calebe Werneck Couto",
+						"https://cwc3d.net", "calebewerneck@hotmail.com"))
 				.build();
-				
-				
-		
 	}
+
+ private  List<SecurityContext> securityContext() {
+	 List<SecurityContext> list = new ArrayList<SecurityContext>();
+	 list.add(SecurityContext.builder().securityReferences(defaultAuth()).build());
+	 return list;
+ }	
+
+ private List<SecurityReference> defaultAuth(){
+	 AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+	 AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+	 authorizationScopes[0] = authorizationScope;
+	 return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
+ }	
 
 }
