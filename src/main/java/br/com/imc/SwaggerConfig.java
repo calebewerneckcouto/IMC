367450bed97a426147a120bbf1bf11e7a97b1615
Calebe <calebewerneck@hotmail.com>
2024-04-16 15:03:07 -0300

    Configurando informações do Programador do Sistema...

--- src/main/java/br/com/imc/SwaggerConfig.java
+++ src/main/java/br/com/imc/SwaggerConfig.java
@@ -8,10 +8,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.builders.ResponseMessageBuilder;
 import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -29,7 +32,8 @@ public class SwaggerConfig {
 				.apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
 				.paths(PathSelectors.any())
 				.build().useDefaultResponseMessages(false)
-				.globalResponseMessage(RequestMethod.GET,this.responseCodeHttp());
+				.globalResponseMessage(RequestMethod.GET,this.responseCodeHttp())
+				.apiInfo(ApiInfo());
 		
 	}
 	
@@ -68,6 +72,20 @@ public class SwaggerConfig {
 		
 		return list;
 		
+		
+	}
+	
+	
+	private ApiInfo  ApiInfo() {
+		
+		return new ApiInfoBuilder().title("Sistema para controle de IMC")
+				.description("Sistema CWC")
+				.version("0.0.1")
+				.licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
+				.contact(new Contact("Calebe Werneck Couto", "http://cwc3d.net", "calebewerneck@hotmail.com"))
+				.build();
+				
+				
 		
 	}
 
